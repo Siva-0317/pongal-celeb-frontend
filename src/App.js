954ad83f787c84a2d76d7c8901d3995af057b551
Siva-0317 <sivabalaji2717@outlook.com>
2026-01-12 23:29:46 +0530

    CSE

--- src/App.js
+++ src/App.js
@@ -17,6 +17,22 @@ function App() {
     <div className="App">
       {/* === LEFT SIDE: Avatar, Logos, Mic === */}
       <div className="left-panel">
+        {/* 🔧 HEADER BAR WITH LOGOS + TEXT */}
+        <div className="header-bar">
+          <img 
+            src="https://i.ibb.co/d4KrJrxv/eec-logo-finalized-1536x516-1.png" 
+            className="header-logo left" 
+            alt="EEC Logo" 
+          />
+          <div className="header-text">Department of CSE</div>
+          <img 
+            src="https://i.ibb.co/wFFkzGVR/ACE.png" 
+            className="header-logo right" 
+            alt="ACE Logo" 
+          />
+        </div>
+
+        {/* AVATAR + MIC */}
         <div className="avatar-wrapper">
           <ChatbotAvatar 
             emotion={emotion} 
